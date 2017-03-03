df649c2513f295e6b81757737f4065337909e952
Brian Schleifer <brianschleifer@gmail.com>
2017-03-03 04:46:04 -0800

    Route for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # Add a root route if you don't have one...
   # We can use users#new for now, or replace this with the controller and action you want to be the site root:
-  root to: 'users#new'
+  root to: 'sessions#new'
 
   # sign up page with form:
   get 'users/new' => 'users#new', as: :new_user
