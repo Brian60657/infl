e6e0bea61dc6779df45d68a4ac241c01024b8b0c
Brian Schleifer <brianschleifer@gmail.com>
2017-03-06 11:53:31 -0800

    Updated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-    root "sessions#new"
+    root "posts#index"
 
     get "users/new" => "users#new", as: :new_user
     get "users/:id" => "users#show", as: :user
